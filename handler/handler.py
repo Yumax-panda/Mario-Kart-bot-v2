1b24f9e0dcecbdf02c0ded843473b715f174364d
Yumax-panda <yuma0wall@gmail.com>
2024-11-23 01:38:44 +0900

    不要なインポートを削除

--- handler/handler.py
+++ handler/handler.py
@@ -14,7 +14,6 @@ from .utility import UtilityHandler
 if TYPE_CHECKING:
     from mk8dx.lounge.types.client import LoungeClient as ILoungeClient
     from repository.config import Config
-    from repository.types.repository import Repository as IRepository
     from service.types.services import Service as IService
 
 
