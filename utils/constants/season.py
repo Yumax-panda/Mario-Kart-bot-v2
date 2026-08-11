9fd21302293ae0ac6234131c7d77b1cea99f4cbd
Yumax-panda <yuma0wall@gmail.com>
2026-08-11 23:51:26 +0900

    update season

--- utils/constants/season.py
+++ utils/constants/season.py
@@ -6,6 +6,6 @@ __all__ = (
     "Season",
 )
 
-Season = Literal[4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+Season = Literal[4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 MIN_SEASON: Final[int] = 4
-CURRENT_SEASON: Final[int] = 15
+CURRENT_SEASON: Final[int] = 16
